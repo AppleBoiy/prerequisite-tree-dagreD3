935165c7319fb43be5f47c4cd46c023059f68708
AppleBoiy <contact.chaipat@gmail.com>
2023-05-23 22:27:07 +0700

    create template repo

--- script/set_shell_theme.sh
+++ script/set_shell_theme.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 
 file_path="$HOME/.zshrc"
 search_phrase='ZSH_THEME'
@@ -11,11 +11,11 @@ temp_file="./temp.txt"
 # Loop through the lines in the file and replace matching lines
 while IFS= read -r line; do
     if [[ "$line" == *"$search_phrase"* ]]; then
-        echo "$replace_line" >> "$temp_file"
+        echo "$replace_line" >>"$temp_file"
     else
-        echo "$line" >> "$temp_file"
+        echo "$line" >>"$temp_file"
     fi
-done < "$file_path"
+done <"$file_path"
 
 # Replace the original file with the modified contents
 mv "$temp_file" "$file_path"
