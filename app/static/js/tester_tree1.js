62c16c3516c689eabae0402e2bcb6265125dd550
AppleBoiy <contact.chaipat@gmail.com>
2023-05-25 21:41:17 +0700

    add node attr

--- app/static/js/tester_tree1.js
+++ app/static/js/tester_tree1.js
@@ -14,98 +14,60 @@ const y4t2 = document.getElementById("y4t2");
 
 // get data from json
 async function getData() {
-  try {
-    const response = await fetch(filePath);
-    const result = await response.json();
-    return result;
-  } catch (error) {console.log(error);}
+  const response = await fetch(filePath);
+  return await response.json();
 }
 
-async function countNode() {
-  const data = await getData();
-  
-}
 // create Node
-function createNode(name) {
+function createNode(data) {
+  const name = data.abbr;
+
   const node = document.createElement("div");
+  const detail = document.createElement("div")
+  const info = document.createElement("p")
+
   node.classList.add('node');
+  node.setAttribute("id", data.code);
+  detail.classList.add('dialog-box')
+
   // node.style.width = maxWidth + "%";
   const text = document.createTextNode(name);
-  node.appendChild(text);
-  return node
+  info.innerText = JSON.stringify(data);
 
+  detail.appendChild(info)
+
+  node.appendChild(text);
+  node.appendChild(detail)
+
+
+  node.classList.add("textbox")
+
+  return node
 }
 // add Node
 async function addNode() {
   const data = await getData();
-  let nodeCount = [0,0,0,0,0,0,0,0];
-  // let y1t1 = 0;
-  // let y1t2 = 0;
-  // let y2t1 = 0;
-  // let y2t2 = 0;
-  // let y3t1 = 0;
-  // let y3t2 = 0;
-  // let y4t1 = 0;
-  // let y4t2 = 0;
-  // count Node
-  data.forEach(element => {
-    switch(element.year){
-      case 1:
-        (element.term == 1) ? nodeCount[0]++ : nodeCount[1]++;
-        break;
-      case 2:
-        (element.term == 1) ? nodeCount[2]++ : nodeCount[3]++;
-        break;
-      case 3:
-        (element.term == 1) ? nodeCount[4]++ : nodeCount[5]++;
-        break;
-      case 4:
-        (element.term == 1) ? nodeCount[6]++ : nodeCount[7]++;
-        break;
-    }
-  })
-  // const nodeWidth = 100 / Math.max(...nodeCount);
 
   // create Node
   data.forEach(element => {
-    const newNode = createNode(element.abbr)
-    switch(element.year){
+
+    const newNode = createNode(element)
+    switch (element.year) {
       case 1:
-        (element.term == 1) ? y1t1.appendChild(newNode) : y1t2.appendChild(newNode);
+        (element.term === 1) ? y1t1.appendChild(newNode) : y1t2.appendChild(newNode);
         break;
       case 2:
-        (element.term == 1) ? y2t1.appendChild(newNode) : y2t2.appendChild(newNode);
+        (element.term === 1) ? y2t1.appendChild(newNode) : y2t2.appendChild(newNode);
         break;
       case 3:
-        (element.term == 1) ? y3t1.appendChild(newNode) : y3t2.appendChild(newNode);
+        (element.term === 1) ? y3t1.appendChild(newNode) : y3t2.appendChild(newNode);
         break;
       case 4:
-        (element.term == 1) ? y4t1.appendChild(newNode) : y4t2.appendChild(newNode);
+        (element.term === 1) ? y4t1.appendChild(newNode) : y4t2.appendChild(newNode);
         break;
     }
   })
-  console.log(nodeCount);
-  
 }
 
-
-async function createList() {
-  const data = await getData();
-  data.forEach(element => {
-    const a = document.createElement("p");
-    a.className = "test"
-    a.innerHTML = `${element.code} ${element["parent list"]} ${element["child list"]} ${element.credit} ${element["full Name"]} ${element.year} ${element.abbr} ${element.term}`
-    root.appendChild(a);
-  });
-}
 
 addNode()
-createList()
-
-
-const test = document.getElementById("testt");
-let path = document.createElementNS(test.namespaceURI,"path");
-path.setAttributeNS(null, "d",`M${100},10 L150,10`);
-path.setAttributeNS(null, "style", "stroke:red; stroke-width: 1.25px; fill: none;marker-end: url(#arrow);");
-test.appendChild(path);
-console.log(path.getBoundingClientRect());
