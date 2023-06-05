bbfece4b48cdd3e169292cdc54910bac0dc2e291
AppleBoiy <contact.chaipat@gmail.com>
2023-06-05 23:04:48 +0700

    create node dictionary

--- app/static/js/tree.js
+++ app/static/js/tree.js
@@ -1,3 +1,210 @@
+const raw_data = [
+  {
+    "abbr": "CS111",
+    "parent": [],
+    "children": ["204114"],
+    "code": "204111",
+    "credit": 3,
+    "full Name": "Fundamentals of Programming",
+    "year": 1,
+    "term": 1
+  },
+  {
+    "code": "206183",
+    "parent": [],
+    "children": ["204451"],
+    "credit": 3,
+    "full Name": "Discrete Structure",
+    "year": 1,
+    "abbr": "Math183",
+    "term": 1
+  },
+  {
+    "code": "204114",
+    "parent": ["204111"],
+    "children": ["204203", "204212", "204231", "204232", "204252"],
+    "credit": 3,
+    "full Name": "Introduction to Object-oriented Programming",
+    "year": 1,
+    "abbr": "CS114",
+    "term": 2
+  },
+  {
+    "code": "204203",
+    "parent": ["204114"],
+    "children": [],
+    "credit": 3,
+    "full Name": "Computer Science Technology",
+    "year": 2,
+    "abbr": "CS203",
+    "term": 1
+  },
+  {
+    "code": "204212",
+    "parent": ["204114"],
+    "children": ["204315", "204361"],
+    "credit": 3,
+    "full Name": "Modern Application Development",
+    "year": 2,
+    "abbr": "CS212",
+    "term": 2
+  },
+  {
+    "code": "204231",
+    "parent": ["204114"],
+    "children": ["204341"],
+    "credit": 3,
+    "full Name": "Computer Organization and Architecture",
+    "year": 2,
+    "abbr": "CS231",
+    "term": 1
+  },
+  {
+    "code": "204232",
+    "parent": ["204114"],
+    "children": ["204390"],
+    "credit": 3,
+    "full Name": "Computer Networks and Protocols",
+    "year": 2,
+    "abbr": "CS232",
+    "term": 2
+  },
+  {
+    "code": "204252",
+    "parent": ["204114"],
+    "children": ["204271", "204321", "204451"],
+    "credit": 3,
+    "full Name": "Data Structures and Analysis",
+    "year": 2,
+    "abbr": "CS252",
+    "term": 1
+  },
+  {
+    "code": "208269",
+    "parent": [],
+    "children": ["204271"],
+    "credit": 3,
+    "full Name": "Statistics for Computer Science",
+    "year": 2,
+    "abbr": "CS252",
+    "term": 1
+  },
+  {
+    // special characters
+    "code": "204271",
+    "parent": ["204252", "208269"],
+    "children": [],
+    "credit": 3,
+    "full Name": "Introduction to Artificial Intelligence",
+    "year": 2,
+    "abbr": "CS271",
+    "term": 2
+  },
+  {
+    "code": "204306",
+    "parent": "Third year standing",
+    "children": "None",
+    "credit": 1,
+    "full Name": "Ethics for Computer Professionals",
+    "year": 3,
+    "abbr": "CS306",
+    "term": 2
+  },
+  {
+    "code": "204315",
+    "parent": ["204212"],
+    "children": "None",
+    "credit": 3,
+    "full Name": "Organization of Programming Languages",
+    "year": 3,
+    "abbr": "CS315",
+    "term": 2
+  },
+  {
+    "code": "204321",
+    "parent": ["204251"],
+    "children": "None",
+    "credit": 3,
+    "full Name": "Database Systems",
+    "year": 3,
+    "abbr": "CS321",
+    "term": 1
+  },
+  {
+    "code": "204341",
+    "parent": ["204231"],
+    "children": ["204390"],
+    "credit": 3,
+    "full Name": "Operating Systems",
+    "year": 3,
+    "abbr": "CS341",
+    "term": 1
+  },
+  {
+    "code": "204361",
+    "parent": ["204212"],
+    "children": ["204390"],
+    "credit": 3,
+    "full Name": "Software Engineering",
+    "year": 3,
+    "abbr": "CS361",
+    "term": 1
+  },
+  {
+    // special characters
+    "code": "204451",
+    "parent": ["204251", ("206183", "206281")],
+    "children": "None",
+    "credit": 3,
+    "full Name": "Algorithm Design and Analysis",
+    "year": 3,
+    "abbr": "CS451",
+    "term": 1
+  },
+  {
+    "code": "204490",
+    "parent": [],
+    "children": "None",
+    "credit": 3,
+    "full Name": "Research in Computer Science",
+    "year": 3,
+    "abbr": "CS490",
+    "term": 2,
+    "condition": "Consent of the department"
+  },
+  {
+    "code": "204390",
+    "parent": ["204232", "204321", "204341", "204361"],
+    "children": "None",
+    "credit": 1,
+    "full Name": "Computer Job Training",
+    "year": 4,
+    "abbr": "CS390",
+    "term": 1
+  },
+  {
+    "code": "204496",
+    "parent": "Fourth year standing and Consent of the department",
+    "children": "None",
+    "credit": 6,
+    "full Name": "Cooperative Education",
+    "year": 4,
+    "abbr": "CS496",
+    "term": 1
+  },
+  {
+    "code": "204497",
+    "parent": "Consent of the department",
+    "children": "None",
+    "credit": 1,
+    "full Name": "Seminar in Computer Science",
+    "year": 4,
+    "abbr": "CS497",
+    "term": 2
+  }
+]
+var dict = {};
+
 const graphGen = () => {
   let graphlib = dagreD3.graphlib;
   let render = dagreD3.render();
@@ -172,61 +379,15 @@ const graphGen = () => {
 // Center the graph
   let offset = PADDING / 2;
   svgGroup.attr("transform", "translate(" + offset + ", " + offset + ")");
+
 }
 graphGen();
 
-function enroll() {
-  // g tag class node
-  const nodes = document.getElementsByClassName("node");
-
-  /*
-    <g class="node y1" id="CS111" transform="translate(320,25)" style="opacity: 1;">
-        <rect rx="10" ry="10" x="-35" y="-25" width="70" height="50"></rect>
-        <g class="label" transform="translate(0,0)">
-            <g transform="translate(-20.36458396911621,-7.6666669845581055)">
-                <text>
-                    <tspan xml:space="preserve" dy="1em" x="1">CS111</tspan>
-                </text>
-            </g>
-        </g>
-    </g>
-  */
-
-  for (let node of nodes) {
-    // child tag in node tag
-    const children = node.childNodes;
-
-    //NodeList(2) [ rect, g.label ]
-    //console.log(children)
-    for (const child of children) {
-      /*
-        rect: <rect rx="10" ry="10" x="-35" y="-25" width="70" height="50"></rect>
-        g.Label:
-        <g transform="translate(-20.36458396911621,-7.6666669845581055)">
-            <text>
-                <tspan xml:space="preserve" dy="1em" x="1">CS111</tspan>
-            </text>
-        </g>
-      */
-
-      // IF CHILD IS RECT PRINT: ELSE DO
-      // <g></g> got "SVGGElement"
-      // <rect></rect> got "SVGRectElement"
-      // if (child.constructor.name === "SVGRectElement") {
-      //   console.log(child);
-      //   // <rect bla bla... ></rect>
-      // } else {
-      //   const text = child.childNodes[0].childNodes[0];
-      //   console.log(text);
-      // }
-    }
-  }
-}
-enroll();
 
 const addAttr = () => {
 
 
+  const nodeInfo = [];
 
   const rectTag = document.getElementsByTagName("rect");
   const textTagArray = document.getElementsByTagName("text");
@@ -240,6 +401,8 @@ const addAttr = () => {
   }
 
   for (const rect of rectTag) {
+
+    nodeInfo.push( { "node":rect.parentNode, "abbr":rect.parentNode.id } )
 
     // temp var to rest old style
     const oldBG = rect.style.fill;
@@ -263,5 +426,16 @@ const addAttr = () => {
     })
   }
 
+  nodeInfo.forEach( (e) => {
+    console.log(e);
+  })
 }
 addAttr();
+
+raw_data.forEach(
+    (e) => {
+      dict[e.abbr] = e
+    }
+)
+
+console.log(dict)
