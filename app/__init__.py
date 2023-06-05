23fb4f257eca8c2e378986c29394718abe8d2a2f
AppleBoiy <contact.chaipat@gmail.com>
2023-06-05 20:33:31 +0700

    Fix security hotspots

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__, static_folder='static')
 
+#the CSRFProtect module should be used (and not disabled further with WTF_CSRF_ENABLED set to false):
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
+
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'df0331cefc6c2b9a5d0208a726a5d1c0fd37324feba25506'
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def home():
     return app.send_static_file("index.html")
