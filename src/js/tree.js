55e40dc8450a9aa7deefdfee252c063144f78242
ICCIE <work.chaipat@gmail.com>
2023-06-10 13:23:30 +0700

    split main function into sub unit

--- src/js/tree.js
+++ src/js/tree.js
@@ -1,45 +1,49 @@
-const main = async () => {
-	async function convertSpreadsheetToJson(response) {
+const constant = {
+	spreadSheetUrl:"https://docs.google.com/spreadsheets/d/1t8dvUUdvOxdiKQv5nagGaHyiw3P-C2o0Qg6C_1Tlq58/edit#gid=0",
+	PADDING:120,
+	offset:60
+}
 
-		// build list of array rows from spreadsheet to jsonData
-		const data = await response.arrayBuffer();
-		const workbook = XLSX.read(data, {type: "array"});
-		const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+/**
+ * get dictionary of course detail from url
+ *
+ * @param url link to spreadSheet
+ * @returns {Promise<Object>}
+ */
+async function spreadsheetToJson(url) {
+	// build list of array rows from spreadsheet to jsonData
+	const response = await fetch(url);
+	const data = await response.arrayBuffer();
+	const workbook = XLSX.read(data, {type: "array"});
+	const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
-		// Define the range of rows and columns
-		const range = XLSX.utils.decode_range(worksheet["!ref"]);
-		range.s.r = 1; // Start from the first row
-		range.e.r = 21; // End at the 20th row
-		range.s.c = 1; // Start from the first column
-		range.e.c = 9; // End at the 8th column
+	// Define the range of rows and columns
+	const range = XLSX.utils.decode_range(worksheet["!ref"]);
+	range.s.r = 1; // Start from the first row
+	range.e.r = 21; // End at the 20th row
+	range.s.c = 1; // Start from the first column
+	range.e.c = 9; // End at the 8th column
 
-		// import data in list from to jsonData
-		const jsonData = XLSX.utils.sheet_to_json(worksheet, {
-			header: 1,
-			range,
-		});
+	// import data in list from to jsonData
+	const jsonData = XLSX.utils.sheet_to_json(worksheet, {
+		header: 1,
+		range,
+	});
 
-		// Convert the jsonData into an array of objects
-		const result = [];
-		for (let i = 1; i < jsonData.length; i++) {
-			const row = jsonData[i];
-			const obj = {};
-			for (let j = 0; j < row.length; j++) {
-				const headerCell = jsonData[0][j];
-				obj[headerCell] = row[j];
-			}
-			result.push(obj);
+	// Convert the jsonData into an array of objects
+	const result = [];
+	for (let i = 1; i < jsonData.length; i++) {
+		const row = jsonData[i];
+		const obj = {};
+		for (let j = 0; j < row.length; j++) {
+			const headerCell = jsonData[0][j];
+			obj[headerCell] = row[j];
 		}
-
-		return result
+		result.push(obj);
 	}
 
-	const spreadSheetUrl = "https://docs.google.com/spreadsheets/d/1t8dvUUdvOxdiKQv5nagGaHyiw3P-C2o0Qg6C_1Tlq58/edit#gid=0";
-	const rawData = await convertSpreadsheetToJson(await fetch(spreadSheetUrl))
-
-
 	//build rawdata to correct format
-	for (const element of rawData) {
+	for (const element of result) {
 		element.code = (element.code).toString();
 
 		element.parent = (element.parent).toString() === "None"?
@@ -49,48 +53,43 @@ const main = async () => {
 			[]:  element.children.toString().split(", ");
 	}
 
-	const rectTag = document.getElementsByTagName("rect");
-	const textTagArray = document.getElementsByTagName("text");
+	return result
+}
 
-	const abbrDict = {
-		"204":"CS",
-		"206":"Math",
-		"208":"Stat"
-	}
+/**
+ * generate tree view of prerequisite tree from JSON like object
+ * @param rawData dictionary of course detail
+ * @returns {Promise<void>}
+ */
+async function generateTree(rawData) {
 
-	const course = {};
-	const rectDict = {};
 
 	const graphlib = dagreD3.graphlib;
 	const render = dagreD3.render();
-	const PADDING = 120;
-
-	const mainTree = new graphlib.Graph();
 
 	// Set up an SVG group so that we can translate the final graph.
 	const svg = d3.select("svg");
 	const svgGroup = svg.append("g");
 
-	// Center the graph
-	const offset = PADDING / 2;
+	const mainTree = new graphlib.Graph();
 
 	// Set an object for the graph label
 	mainTree.setGraph({"ranksep": 100});
 
 	// Default to assigning a new object as a label for each new edge.
 	mainTree.setDefaultEdgeLabel(() => {
-			return {};
-		});
+		return {};
+	});
 
 	for (const subject of rawData) {
 		// create node
 		mainTree.setNode( subject.code, {
-				label: subject.abbr,
-				width: 50,
-				height: 30,
-				id: subject.abbr,
-				class: `y${subject.year}`,
-			})
+			label: subject.abbr,
+			width: 50,
+			height: 30,
+			id: subject.abbr,
+			class: `y${subject.year}`,
+		})
 		// create line
 		subject.children.forEach( child => {
 			mainTree.setEdge(
@@ -102,18 +101,51 @@ const main = async () => {
 
 	// Round the corners of the nodes
 	mainTree.nodes().forEach( (v) => {
-			const node = mainTree.node(v);
-			node.rx = node.ry = 10;
-		} );
+		const node = mainTree.node(v);
+		node.rx = node.ry = 10;
+	} );
 
 	render(d3.select("svg g"), mainTree);
 
 	// Size the container to the graph
-	svg.attr("width", mainTree.graph().width + PADDING);
-	svg.attr("height", mainTree.graph().height + PADDING);
+	svg.attr("width", mainTree.graph().width + constant.PADDING);
+	svg.attr("height", mainTree.graph().height + constant.PADDING);
 
-	svgGroup.attr("transform", "translate(" + offset + ", " + offset + ")");
+	svgGroup.attr("transform", "translate(" + constant.offset + ", " + constant.offset + ")");
 
+}
+
+/**
+ * rectDivs: HTMLCollection of SVGElement <Rectangular>
+ * textDivs: HMTLCollection of SVGTextElement <text ...></text>
+ *
+ * @returns { Promise<{
+ * 		rectDivs: HTMLCollectionOf<SVGElementTagNameMap[string]>,
+ * 		textDivs: HTMLCollectionOf<SVGElementTagNameMap[string]>
+ * 		}>}
+ */
+async function collectHTMLElement() {
+	return {
+		rectDivs: document.getElementsByTagName("rect"),
+		textDivs: document.getElementsByTagName("text")
+	}
+}
+
+
+const main = async () => {
+	const rawData = await spreadsheetToJson(constant.spreadSheetUrl)
+	const abbrDict = {
+		"204":"CS",
+		"206":"Math",
+		"208":"Stat"
+	}
+
+	const course = {};
+	const rectDict = {};
+
+	generateTree(rawData).then(
+		() => console.log("Main tree generated!")
+	).catch( r => console.log(r) )
 
 	// create dictionary that abbr is key
 	for (const subject of rawData) {
@@ -121,16 +153,24 @@ const main = async () => {
 		rectDict[subject.abbr] = {};
 	}
 
+	const HTMLElement = await collectHTMLElement();
+	const rectTag = HTMLElement.rectDivs;
+	const textTagArray = HTMLElement.textDivs;
+
 	// filter to get only text tag that contains label tag and store to course dictionary
 	for (const textTag of textTagArray) {
 		if (textTag.parentNode.classList[0] === "label")  continue;
 
 		const abbr = textTag.childNodes[0]?.innerHTML;
+		if (!abbr) continue;
+
 		rectDict[abbr]["textDiv"] = textTag;
 	}
 
 	for (const rect of rectTag) {
 		const abbr = rect.parentNode.id;
+		if (!abbr) continue;
+
 		rectDict[abbr]["rectDiv"] = rect;
 		rectDict[abbr]["oldFill"] = rect.style.fill;
 		rectDict[abbr]["oldStroke"] = rect.style.stroke;
@@ -138,8 +178,8 @@ const main = async () => {
 
 	// add child and parent rect to dictionary
 	for (const subject of rawData) {
-		const parent = subject.parent
-		if (typeof parent !== "undefined" || parent?.length !== 0) {
+		let parent = subject.parent
+		if (parent?.length !== 0) {
 			// collect parent node
 			rectDict[subject.abbr]["parentRect"] = parent.map(
 				(e) => {
@@ -149,8 +189,8 @@ const main = async () => {
 			)
 		}
 
-		const child = subject.children
-		if (typeof child !== "undefined" || child?.length !== 0) {
+		let child = subject.children
+		if (child?.length !== 0) {
 			rectDict[subject.abbr]["childRect"] = child.map(
 				(e) => {
 					const abbr = `${abbrDict[e.slice(0, 3)]}${e.slice(3)}`
@@ -183,6 +223,6 @@ const main = async () => {
 
 }
 main().then(
-	() => console.log("Tree generated!")
+	() => console.log("Run main..")
 ).catch( r => console.log(r))
 
